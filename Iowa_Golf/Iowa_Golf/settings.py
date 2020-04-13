cbc6662ff41377141765bfea3289dc778b5e554b
Aaron Holmes <HolmesAaron@Johndeere.com>
2020-04-13 15:31:40 -0500

    Serializers

--- Iowa_Golf/Iowa_Golf/settings.py
+++ Iowa_Golf/Iowa_Golf/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Iowagolfapp',
+    'rest_framework'
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'Iowa_Golf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iowa_golf',
+        'USER': 'iowagolfuser',
+        'PASSWORD': 'iowagolfuser',
+        'HOST': 'localhost'
     }
 }
 
---
+++ Iowa_Golf/Iowagolfapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IowagolfappConfig(AppConfig):
+    name = 'Iowagolfapp'
