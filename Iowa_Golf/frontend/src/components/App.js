afa15806387e6b1167ff5ca2a40379492c33e888
Jared Helming <helmingjared@johndeere.com>
2020-04-14 12:04:25 -0500

    React Setup

---
+++ Iowa_Golf/frontend/src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReatDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return <h1> React App</h1>
+    }
+}
+ReatDOM.render(<App />, document.getElementById('app'));
